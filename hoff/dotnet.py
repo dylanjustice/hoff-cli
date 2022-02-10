5ac0241f73f1decf49cfad913b3bed8f713a6b29
Dylan Justice <dylan.justice@lexisnexis.com>
2022-02-10 08:02:53 -0500

    Rework command structure

--- hoff/dotnet.py
+++ hoff/dotnet.py
@@ -6,51 +6,42 @@ import click
 import subprocess
 import os
 
-
 run_cmd = ["dotnet", "run", "--no-restore"]
 build_cmd = ["dotnet", "build", "--no-restore"]
 
-
-@click.group(invoke_without_command=True)
-@click.option("-b", "--build", is_flag=True)
-@click.option("-c", "--clean", is_flag=True)
-@click.option("-R", "--restore", is_flag=True)
-@click.option("-w", "--watch", is_flag=True)
-@click.argument("path", required=False)
-@click.pass_context
-def dotnet(ctx, restore, clean, build, watch, path):
+@click.group()
+def dotnet():
   """Run various dotnet commands for the project"""
-  if ctx.invoked_subcommand is None:
-    run(restore, clean, build, path)
-  pass
-
 
 @dotnet.command()
 def info():
   """Display .NET information"""
   subprocess.run("dotnet --info")
 
-
-@dotnet.command(name="list-sdks")
+@dotnet.command(name="sdks")
 def list_sdks():
   """Display install SDKs"""
   subprocess.run("dotnet --list-sdks")
 
-
-@dotnet.command(name="list-runtimes")
+@dotnet.command(name="runtimes")
 def list_runtimes():
     """Display installed runtimes"""
     subprocess.run("dotnet --list-runtimes")
 
-
-def run(restore, clean, build, path):
+@dotnet.command(name="run")
+@click.option("-b", "--build", is_flag=True)
+@click.option("-c", "--clean", is_flag=True)
+@click.option("-R", "--restore", is_flag=True)
+@click.option("-w", "--watch", is_flag=True)
+@click.argument("path", required=False)
+def run(restore, clean, build, path, watch):
+  """Run a .NET project"""
   cwd = os.getcwd()
   if path:
     os.chdir(path)
 
   solution_file = solution_path()
   path = solution_path()
-
 
   if solution_file is None:
     error("Solution file not found")
