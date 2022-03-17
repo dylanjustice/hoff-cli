32e90ce2f0e12e9c3a1047e6fb15748d0361d8d8
Dylan Justice <dylan.justice@lexisnexis.com>
2022-03-17 11:18:57 -0400

    Adding tests for node clean

--- modules/node_clean.py
+++ modules/node_clean.py
@@ -1,19 +1,22 @@
 from os import rmdir
+import shutil
 from models.result import Result
 from modules.echo import error, info
 
 
 class NodeClean:
+    ERR_UNABLE_TO_DELETE = "Error deleting node_modules"
+
     @classmethod
-    def run(path: str) -> Result:
+    def run(self, path: str) -> Result:
         node_modules = path / "node_modules"
 
-        info("Recursively deleting {node_modules}".format(node_modules))
+        info("Recursively deleting {0}".format(node_modules))
         try:
-            rmdir(node_modules)
-        except:
+            shutil.rmtree(node_modules, ignore_errors=True)
+        except Exception as ex:
             error("Exited with error")
-            return Result(1, "Error deleting node_modules")
+            return Result(1, self.ERR_UNABLE_TO_DELETE, ex)
 
         return Result(0)
 
---
+++ tests/modules/unit/test_node_clean.py
@@ -0,0 +1,42 @@
+from genericpath import exists
+import os
+from pathlib import Path
+from re import sub
+import shutil
+from tkinter.tix import Tree
+
+from faker import Faker
+
+from modules.node_clean import NodeClean
+
+tmp_path = Path("tmp")
+
+
+def test_run_when_node_modules_does_not_exist_then_returns_without_error(tmp_path: Path):
+    # Arrange & Act
+    result = NodeClean.run(path=tmp_path)
+
+    # Assert
+    assert result.hasError() == False
+
+def test_run_removes_directory_and_returns_without_error(tmp_path: Path):
+    # Arrange
+    fake = Faker()
+    Faker.seed(20)
+    node_modules = tmp_path / "node_modules"
+    node_modules.mkdir()
+    count = fake.random_int(min=2, max=10)
+    for i in range(count):
+        print(i)
+        depth = fake.random_int(min=1, max=10)
+        path = fake.file_path(depth)
+        submodule =  Path(node_modules.as_posix() + path)
+        dir_name = Path(os.path.dirname(submodule.as_posix()))
+        dir_name.mkdir(parents=True, exist_ok=True)
+        submodule.touch()
+    # Act
+    result = NodeClean.run(path=tmp_path)
+
+    # Assert
+    assert result.hasError() == False
+    assert exists(node_modules) == False
